669193befd04260f77d2a2e7bcf01069c9fbcc5d
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-12-20 23:47:36 +0800

    chore: update contact section description

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -14,11 +14,10 @@ const Contact = (props: Props) => {
             <div className="container text-center">
                 <div className="mb-7 md:mb-12 2xl:mb-16">
                     <h2>Get in Touch</h2>
-                    <p className="max-w-[560px] mx-auto 2xl:text-lg">
-                        Whether you &#39;re interested in a collaboration, have
-                        a project in mind, or just want to say hi, feel free to
-                        reach out. Fill out the form below or drop me a message
-                        at{" "}
+                    <p className="max-w-[590px] mx-auto 2xl:text-lg">
+                        Interested in a collaboration or have a project in mind?
+                        Feel free to reach out. Fill out the form below or drop
+                        me a message at{" "}
                         <a
                             href={`mailto:${devMail}`}
                             className="text-purple-100 underline underline-offset-2"
