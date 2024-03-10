a772065c4792c66f53720e7ce837452eacddce53
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2024-03-11 00:37:14 +0800

    refactor: use the latest Image component

--- src/components/ui/project-card.tsx
+++ src/components/ui/project-card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/legacy/image";
+import Image from "next/image";
 import { Chip } from "@/components/ui";
 
 export type ProjectCardProps = {
@@ -25,9 +25,9 @@ const ProjectCard = ({
                 <Image
                     src={thumbnail}
                     alt={`${title} Thumbnail`}
-                    width={560}
-                    height={372}
-                    layout="responsive"
+                    width={1120}
+                    height={744}
+                    className="w-full h-auto"
                 />
                 <div className="project-links bg-purple-100 py-2 px-4 text-white-200">
                     <div className="flex space-x-2 justify-end">
