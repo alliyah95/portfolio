ad2ea9736677a47d2e91ececdc6a5082bf3b49b3
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2024-03-11 00:32:22 +0800

    chore: add explicit height and width to images

--- src/components/ui/skill-item.tsx
+++ src/components/ui/skill-item.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export type SkillItemProps = {
     src: string;
@@ -7,12 +8,13 @@ export type SkillItemProps = {
 
 const SkillItem = ({ src, title }: SkillItemProps) => {
     return (
-        <div className="skill-item m-2 flex flex-col justify-center">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
+        <div className="skill-item m-2 flex flex-col justify-center w-auto">
+            <Image
                 src={src}
                 alt={`${title} Logo`}
-                className="h-16 md:h-20 lg:h-24"
+                className="h-16 md:h-20 lg:h-24 w-auto"
+                width={128}
+                height={128}
             />
             <p className="skill-label">{title}</p>
         </div>
